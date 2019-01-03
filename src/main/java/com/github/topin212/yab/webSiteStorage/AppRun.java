aa1be72a129e72f3c4706504b58be69f590bf628
topin212 <topin2122@gmail.com>
2019-01-03 16:41:01 +0200

    introduced migrations

--- src/main/java/com/github/topin212/yab/webSiteStorage/AppRun.java
+++ src/main/java/com/github/topin212/yab/webSiteStorage/AppRun.java
@@ -1,9 +1,11 @@
 package com.github.topin212.yab.webSiteStorage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AppRun {
 
 	public static void main(String[] args) {
