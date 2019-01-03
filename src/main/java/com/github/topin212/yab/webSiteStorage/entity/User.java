f484edce4398b0ca805a4d9c371030b3bbc76f3d
aleksandr <sashabogush@gmail.com>
2019-01-03 14:51:37 +0200

    Entity User

--- src/main/java/com/github/topin212/yab/webSiteStorage/entity/User.java
+++ src/main/java/com/github/topin212/yab/webSiteStorage/entity/User.java
@@ -1,6 +1,64 @@
 package com.github.topin212.yab.webSiteStorage.entity;
 
+
+
 public class User {
-    private String firstName;
-    private String secondName;
+
+    private long id;
+    private String first_name;
+    private String last_name;
+    private String phone;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(long id, String first_name, String last_name, String phone, String email) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
